6389bde18805fde1664bf322339793520e600e7e
wlm2012 <541253785@qq.com>
2020-05-09 15:33:26 +0800

    OldDate

---
+++ src/main/java/com/test/study/util/time/OldDate.java
@@ -0,0 +1,48 @@
+package com.test.study.util.time;
+
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * OldStringToDate
+ */
+public class OldDate {
+
+    public static Date  stringToDate(String s, String format) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        Date date = sdf.parse(s);
+        return date;
+    }
+
+    public static String dateToString(Date date, String format) {
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        String s = sdf.format(date);
+        return s;
+    }
+
+    public static Timestamp stringToTimestamp(String s) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        Date date = sdf.parse(s);
+        s = sdf.format(date);
+        // 注：String的类型必须形如： yyyy-mm-dd hh:mm:ss[.f...] 这样的格式，中括号表示可选，否则报错！！
+        return Timestamp.valueOf(s);
+    }
+
+    public static String timestampToString(Timestamp ts, String format) {
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return sdf.format(ts);
+    }
+
+    public static Date timestampToDate(Timestamp ts) {
+        return new Date(ts.getTime());
+    }
+
+    public static Timestamp dateToTimestamp(Date date) {
+        return new Timestamp(date.getTime());
+    }
+
+
+
+}
--- src/test/java/com/test/study/AppTest.java
+++ src/test/java/com/test/study/AppTest.java
@@ -1,11 +1,15 @@
 package com.test.study;
 
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.util.Date;
 import java.util.ServiceLoader;
 
 import com.test.study.util.arrays.ArraysTest;
 import com.test.study.util.innerclass.OutClass;
 import com.test.study.util.proxy.pay;
 import com.test.study.util.stream.StreamTest;
+import com.test.study.util.time.OldDate;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,7 +19,22 @@ import org.junit.jupiter.api.Test;
 public class AppTest {
     /**
      * Rigorous Test :-)
+     * 
+     * @throws ParseException
      */
+
+    @Test
+    public void oldTimeTest() throws ParseException {
+        
+        System.out.println(OldDate.stringToDate("2020-01-31 21:59:59","yyyy-MM-dd hh:mm:ss"));
+        System.out.println(OldDate.dateToString(new Date(),"yyyy-MM-dd hh:mm:ss"));
+        System.out.println(OldDate.stringToTimestamp("2020-01-31 21:59:59"));
+        System.out.println(OldDate.timestampToString(new Timestamp(System.currentTimeMillis()),"yyyy-MM-dd hh:mm:ss"));
+        System.out.println(OldDate.timestampToDate(new Timestamp(System.currentTimeMillis())));
+        System.out.println(OldDate.dateToTimestamp(new Date()));
+    }
+
+
 
     @Test
     public void arraysizeTest() {
