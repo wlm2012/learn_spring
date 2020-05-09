de4acb4249bdcfe8bc7a12ee56f3079d4e015607
wlm2012 <541253785@qq.com>
2020-05-09 10:01:41 +0800

    ScheduledTasks

--- src/main/java/com/test/study/StudyApplication.java
+++ src/main/java/com/test/study/StudyApplication.java
@@ -2,8 +2,10 @@ package com.test.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class StudyApplication {
 
 	public static void main(String[] args) {
