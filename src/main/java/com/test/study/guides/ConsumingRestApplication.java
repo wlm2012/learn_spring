a0fd06f23e492c2bed6c013e2e66b730c392a8a4
wlm2012 <541253785@qq.com>
2020-05-11 21:09:40 +0800

    problems

--- src/main/java/com/test/study/guides/ConsumingRestApplication.java
+++ src/main/java/com/test/study/guides/ConsumingRestApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
+// import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 // @Component
