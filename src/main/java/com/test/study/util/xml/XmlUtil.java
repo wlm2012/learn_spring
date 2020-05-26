482326dff506d0261c058b746144872522936918
wlm2012 <541253785@qq.com>
2020-05-26 17:00:11 +0800

    StreamTest

--- src/main/java/com/test/study/util/xml/XmlUtil.java
+++ src/main/java/com/test/study/util/xml/XmlUtil.java
@@ -1,8 +1,38 @@
 package com.test.study.util.xml;
 
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * @author lenovo2
  */
 public class XmlUtil {
+
+	public static void xmlParse(File file) throws ParserConfigurationException, IOException, SAXException {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		Document document = builder.parse(file);
+		Element root = document.getDocumentElement();
+		NodeList children = root.getChildNodes();
+		for (int i = 0; i < children.getLength(); i++) {
+			Node child = children.item(i);
+			if (child instanceof Element) {
+				Element childElement = (Element) child;
+				Text textNode = (Text) childElement.getFirstChild();
+				String text = textNode.getData().trim();
+				if (childElement.getTagName().equals("item_list")) {
+					System.out.println(text);
+				}
+
+			}
+		}
+	}
+
 
 }
--- src/test/java/com/test/study/App.java
+++ src/test/java/com/test/study/App.java
@@ -3,41 +3,51 @@ package com.test.study;
 import com.test.study.util.stream.StreamTest;
 
 import com.test.study.util.work.XmlOutput;
+import com.test.study.util.xml.XmlUtil;
 import javassist.bytecode.CodeIterator;
 import org.junit.jupiter.api.Test;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.IOException;
 
 /**
  * Hello world!
- *
  */
 public class App {
-    protected String s = "";
+	protected String s = "";
 
-    /**
-     * 与0xff相交，可以去除8位以上的数据
-     */
+	/**
+	 * 与0xff相交，可以去除8位以上的数据
+	 */
 
-    @Test
-    public void streamTest() {
-        // StreamTest.test();
-        StreamTest.CollectorsStream();
-    }
+	@Test
+	public void streamTest() {
+		// StreamTest.test();
+		StreamTest.CollectorsStream();
+	}
 
-    @Test
-    public void test() {
-        // Byte b=1;
-        byte value = (byte) 0xff;
-        int value1 = 0xff;
-        System.out.println(value);
-        System.out.println(value1);
+	@Test
+	public void test() {
+		// Byte b=1;
+		byte value = (byte) 0xff;
+		int value1 = 0xff;
+		System.out.println(value);
+		System.out.println(value1);
 
-    }
+	}
 
-    @Test
-    public void xmlOutputTest() throws Exception {
-        XmlOutput.readFile();
-    }
+	@Test
+	public void xmlOutputTest() throws Exception {
+		XmlOutput.readFile();
+	}
+
+	@Test
+	public void xmlUtilTest() throws IOException, SAXException, ParserConfigurationException {
+		String path = "C:\\Users\\lenovo2\\Desktop\\v6\\t_fund_storage_plan_det.xml";
+		File file = new File(path);
+		XmlUtil.xmlParse(file);
+	}
 
 }
