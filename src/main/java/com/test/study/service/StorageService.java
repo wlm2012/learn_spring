8933054cd27178924bfab1ca3a3c0e03dc24a313
wlm2012 <541253785@qq.com>
2020-06-19 22:55:10 +0800

    update

--- src/main/java/com/test/study/service/StorageService.java
+++ src/main/java/com/test/study/service/StorageService.java
@@ -1,6 +1,26 @@
 package com.test.study.service;
 
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+/**
+ * @author wlm
+ */
 public interface StorageService {
 
+    void init();
+
+    void store(MultipartFile file);
+
+    Stream<Path> loadAll();
+
+    Path load(String filename);
+
+    Resource loadAsResource(String filename);
+
+    void deleteAll();
 
 }
