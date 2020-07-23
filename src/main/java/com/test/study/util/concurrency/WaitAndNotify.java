7a18b66e0a73660bd65035361abc662f35454115
wlm2012 <541253785@qq.com>
2020-07-23 20:16:38 +0800

    WaitAndNotify

---
+++ src/main/java/com/test/study/util/concurrency/WaitAndNotify.java
@@ -0,0 +1,48 @@
+package com.test.study.util.concurrency;
+
+public class WaitAndNotify {
+    private static final Object lock = new Object();
+
+    static class ThreadA implements Runnable {
+        @Override
+        public void run() {
+            synchronized (lock) {
+                for (int i = 0; i < 5; i++) {
+                    try {
+                        System.out.println("ThreadA: " + i);
+                        lock.notify();
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                lock.notify();
+            }
+        }
+    }
+
+    static class ThreadB implements Runnable {
+        @Override
+        public void run() {
+            synchronized (lock) {
+                for (int i = 0; i < 5; i++) {
+                    try {
+                        System.out.println("ThreadB: " + i);
+                        lock.notify();
+                        lock.wait();
+                        System.out.println("ThreadB1: " + i);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                lock.notify();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(new ThreadA()).start();
+        Thread.sleep(1000);
+        new Thread(new ThreadB()).start();
+    }
+}
