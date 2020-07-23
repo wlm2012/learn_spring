703d1c4c0bcbb2494f8429a66d303c3a312af4b7
wlm2012 <541253785@qq.com>
2020-07-23 19:56:11 +0800

    task

--- src/main/java/com/test/study/util/concurrency/InterruptTest.java
+++ src/main/java/com/test/study/util/concurrency/InterruptTest.java
@@ -10,16 +10,27 @@ public class InterruptTest {
     }
 
     public static void test() throws InterruptedException {
-        Runnable r=()->{
+        Runnable r = () -> {
+
+            System.out.println("rr");
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
             System.out.println("r");
+
         };
-        Thread thread=new Thread(r);
+        Thread thread = new Thread(r);
         thread.start();
         //注意Thread
 //        Thread.sleep(1000);
         thread.interrupt();
         System.out.println(thread.isInterrupted());
         System.out.println(thread.isInterrupted());
-        System.out.println(thread.getName()+"   "+thread.getState());
+        System.out.println(thread.interrupted());
+        System.out.println(thread.isInterrupted());
+        System.out.println(thread.getName() + "   " + thread.getState());
     }
 }
