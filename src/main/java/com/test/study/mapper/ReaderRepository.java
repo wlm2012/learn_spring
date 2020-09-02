4d6205da4a8dda45005e303424b9444150a6d729
wlm2012 <541253785@qq.com>
2020-09-02 20:52:02 +0800

    format

--- src/main/java/com/test/study/mapper/ReaderRepository.java
+++ src/main/java/com/test/study/mapper/ReaderRepository.java
@@ -2,9 +2,7 @@ package com.test.study.mapper;
 
 import com.test.study.entity.Reader;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 
 public interface ReaderRepository extends JpaRepository<Reader, String> {
-
 }
