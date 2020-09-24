2f128e953f4af6324535831662851ce8e5df355a
wlm2012 <541253785@qq.com>
2020-09-24 20:27:18 +0800

    unfinished

--- src/main/java/com/test/study/util/concurrency/LockTest.java
+++ src/main/java/com/test/study/util/concurrency/LockTest.java
@@ -4,12 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.TimeUnit;
+
 
 @RestController
 public class LockTest {
 
 	@RequestMapping("/firstThread")
-	public static synchronized void firstThread(){
+	public static synchronized void firstThread() {
 		System.out.println("firstThread");
 		try {
 			Thread.sleep(5000);
@@ -20,7 +24,7 @@ public class LockTest {
 	}
 
 	@RequestMapping("/secondThread")
-	public synchronized void secondThread(){
+	public synchronized void secondThread() {
 		System.out.println("secondThread");
 		try {
 			Thread.sleep(5000);
@@ -31,7 +35,7 @@ public class LockTest {
 	}
 
 	@RequestMapping("/thirdThread")
-	public static synchronized void thirdThread(){
+	public static synchronized void thirdThread() {
 		System.out.println("thirdThread");
 		try {
 			Thread.sleep(5000);
@@ -39,5 +43,20 @@ public class LockTest {
 			e.printStackTrace();
 		}
 		System.out.println("thirdThread");
+	}
+
+
+	public static void fourthThread() {
+		Instant startTime = Instant.now();
+		Thread thread = new Thread(() -> {
+			try {
+				TimeUnit.SECONDS.sleep(5);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+
+
+		System.out.println("time= " + Duration.between(startTime, Instant.now()).toMillis());
 	}
 }
