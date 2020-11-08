74751734bafb75149b094794d3ad44aa66716ab5
wlm2012 <541253785@qq.com>
2020-11-08 17:25:45 +0800

    jdbcTemplate

--- src/main/java/com/test/study/guides/RelationalDataAccessApplication.java
+++ src/main/java/com/test/study/guides/RelationalDataAccessApplication.java
@@ -19,35 +19,39 @@ import org.springframework.stereotype.Component;
 //@Component
 public class RelationalDataAccessApplication implements CommandLineRunner {
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
-    private static final Logger log = LoggerFactory.getLogger(RelationalDataAccessApplication.class);
+	private static final Logger log = LoggerFactory.getLogger(RelationalDataAccessApplication.class);
 
-    @Override
-    public void run(String... args) throws Exception {
+	@Override
+	public void run(String... args) throws Exception {
 
-        log.info("Creating tables");
+		log.info("Creating tables");
 
-        // DROP TABLE customers IF EXISTS 改为 "DROP TABLE  IF EXISTS customers" ，不然alibaba druid 连接池报错
-        jdbcTemplate.execute("DROP TABLE  IF EXISTS customers");
-        jdbcTemplate.execute("CREATE TABLE customers(" + "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+		// DROP TABLE customers IF EXISTS 改为 "DROP TABLE  IF EXISTS customers" ，不然alibaba druid 连接池报错
+		jdbcTemplate.execute("DROP TABLE  IF EXISTS customers");
+		jdbcTemplate.execute("CREATE TABLE customers(" + "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
 
-        // Split up the array of whole names into an array of first/last names
-        List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
-                .map(name -> name.split(" ")).collect(Collectors.toList());
+		// Split up the array of whole names into an array of first/last names
+		List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
+				.map(name -> name.split(" ")).collect(Collectors.toList());
 
-        // Use a Java 8 stream to print out each tuple of the list
-        splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
+		// Use a Java 8 stream to print out each tuple of the list
+		splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
 
-        // Uses JdbcTemplate's batchUpdate operation to bulk load data
-        jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
+		// Uses JdbcTemplate's batchUpdate operation to bulk load data
+		jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
 
-        log.info("Querying for customer records where first_name = 'Josh':");
-        jdbcTemplate.query("SELECT id, first_name, last_name FROM customers WHERE first_name = ?",
-                new Object[] { "Josh" },
-                (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name")))
-                .forEach(customer -> log.info(customer.toString()));
-    }
+		log.info("Querying for customer records where first_name = 'Josh':");
+		jdbcTemplate.query("SELECT id, first_name, last_name FROM customers WHERE first_name = ?",
+				new Object[]{"Josh"},
+				(rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name")))
+				.forEach(customer -> log.info(customer.toString()));
+
+		List<Customer> customers = jdbcTemplate.query("SELECT id, first_name, last_name FROM customers WHERE first_name = ?",
+				new Object[]{"Josh"},
+				(rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name")));
+	}
 
 }
