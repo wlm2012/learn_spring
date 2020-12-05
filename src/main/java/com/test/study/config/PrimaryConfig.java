1dc226df74b9a7438363f8a099eb32e5daa69044
wlm2012 <541253785@qq.com>
2020-12-05 17:23:38 +0800

    config

--- src/main/java/com/test/study/config/PrimaryConfig.java
+++ src/main/java/com/test/study/config/PrimaryConfig.java
@@ -2,27 +2,82 @@ package com.test.study.config;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories(
 		entityManagerFactoryRef = "entityManagerFactoryPrimary",
-		transactionManagerRef="transactionManagerPrimary",
-		basePackages= { "com.test.study.primaryMapper" }
+		transactionManagerRef = "transactionManagerPrimary",
+		basePackages = {"com.test.study.primaryMapper"}
 )
 public class PrimaryConfig {
 
 
 	private Environment env;
 
+	private DataSource dataSource;
+
 	@Autowired
-	public void setEnvironment(Environment env){
-		this.env=env;
+	public void setEnvironment(Environment env) {
+		this.env = env;
+	}
+
+	@Autowired
+	@Qualifier("primaryDataSource")
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	@Primary
+	@Bean(name = "entityManagerPrimary")
+	public EntityManager entityManager(EntityManagerFactoryBuilder builder) {
+		return entityManagerFactoryPrimary(builder).getObject().createEntityManager();
+	}
+
+	@Primary
+	@Bean(name = "entityManagerFactoryPrimary")
+	public LocalContainerEntityManagerFactoryBean entityManagerFactoryPrimary (EntityManagerFactoryBuilder builder) {
+		return builder
+				.dataSource(dataSource)
+				.properties(getVendorProperties())
+				.packages("xin.zhuyao.springbootjapdatasource.domain.primary")
+				.persistenceUnit("primaryPersistenceUnit")
+				.build();
 	}
 
 
+
+	private Map<String, String> getVendorProperties() {
+		Map<String, String> jpaProperties = new HashMap<>(16);
+		jpaProperties.put("hibernate.hbm2ddl.auto", "update");
+		jpaProperties.put("hibernate.show_sql", env.getProperty("spring.jpa.show-sql"));
+		jpaProperties.put("hibernate.format_sql", env.getProperty("spring.jpa.hibernate.format_sql"));
+		jpaProperties.put("hibernate.dialect", env.getProperty("spring.jpa.hibernate.primary-dialect"));
+		jpaProperties.put("hibernate.current_session_context_class", "org.springframework.orm.hibernate5.SpringSessionContext");
+		return jpaProperties;
+	}
+
+	@Primary
+	@Bean(name = "transactionManagerPrimary")
+	public PlatformTransactionManager transactionManagerPrimary(EntityManagerFactoryBuilder builder) {
+		return new JpaTransactionManager(entityManagerFactoryPrimary(builder).getObject());
+	}
 }
+
+
