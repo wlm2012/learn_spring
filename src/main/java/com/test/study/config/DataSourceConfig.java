736c59e409b66413ab1c10d39bb8422e104f2abe
wlm2012 <541253785@qq.com>
2020-12-05 15:42:23 +0800

    config

--- src/main/java/com/test/study/config/DataSourceConfig.java
+++ src/main/java/com/test/study/config/DataSourceConfig.java
@@ -2,6 +2,7 @@ package com.test.study.config;
 
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionManager;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
@@ -29,10 +31,18 @@ public class DataSourceConfig {
 	}
 
 
+/*	@Bean
+	@Resource
+	@Primary
+	public PlatformTransactionManager primaryPlatformTransactionManager() {
+		return new DataSourceTransactionManager(primaryDataSource());
+	}*/
+
 	@Bean
 	@Resource
 	@Primary
-	public PlatformTransactionManager primaryTransactionManager() {
+	@Qualifier("transactionManager")
+	public TransactionManager primaryTransactionManager() {
 		return new DataSourceTransactionManager(primaryDataSource());
 	}
 
--- src/main/java/com/test/study/controller/TPersInfoController.java
+++ src/main/java/com/test/study/controller/TPersInfoController.java
@@ -1,4 +1,33 @@
 package com.test.study.controller;
 
+
+import com.test.study.entity.PersInfo;
+import com.test.study.mapper.PersInfoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
 public class TPersInfoController {
+
+
+	private PersInfoRepository persInfoRepository;
+
+	@Autowired
+	public void setRepository(PersInfoRepository repository) {
+		persInfoRepository = repository;
+	}
+
+
+	@RequestMapping("/TPersInfo/{zjhm}")
+	public PersInfo tPersInfo(@PathVariable String zjhm) {
+		Optional<PersInfo> persInfo = persInfoRepository.findById(zjhm);
+		if (persInfo.isPresent()) {
+			return persInfo.get();
+		}
+		return null;
+	}
 }
