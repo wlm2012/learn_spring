1631d5eb457dd50fc4c200bd1cec799acfeb47a2
wlm2012 <541253785@qq.com>
2020-12-14 19:37:01 +0800

    threadName

--- src/main/java/com/test/study/util/concurrency/ThreadLocalTest.java
+++ src/main/java/com/test/study/util/concurrency/ThreadLocalTest.java
@@ -1,26 +1,40 @@
 package com.test.study.util.concurrency;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
+@RestController
 public class ThreadLocalTest {
 
-    public static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+	public static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
+	public ThreadLocal<String> threadName = ThreadLocal.withInitial(() -> null);
 
-    public static void simpleDateFormatTest() {
+	public static void simpleDateFormatTest() {
 
-        Runnable runnable = () -> {
-            for (int i = 0; i < 1000; i++) {
-                String date = dateFormat.format(new Date());
-                System.out.println(date);
-            }
-        };
+		Runnable runnable = () -> {
+			for (int i = 0; i < 1000; i++) {
+				String date = dateFormat.format(new Date());
+				System.out.println(date);
+			}
+		};
 
-        for (int i = 0; i < 10000; i++) {
-            Thread thread = new Thread(runnable);
-            thread.start();
-        }
+		for (int i = 0; i < 10000; i++) {
+			Thread thread = new Thread(runnable);
+			thread.start();
+		}
 
-    }
+	}
+
+	@RequestMapping("/threadName")
+	public void threadLocalTest() {
+		System.out.println("before:" + Thread.currentThread().getName() + ":" + threadName.get());
+		threadName.set(Thread.currentThread().getName());
+		System.out.println("after:" + Thread.currentThread().getName() + ":" + threadName.get());
+
+	}
 }
