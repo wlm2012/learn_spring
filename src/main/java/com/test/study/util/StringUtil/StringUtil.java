f8c87ed89e9c9d38cef1793dd10c206fb77908f6
wlm2012 <541253785@qq.com>
2021-01-07 20:14:40 +0800

    bug fix

--- src/main/java/com/test/study/util/StringUtil/StringUtil.java
+++ src/main/java/com/test/study/util/StringUtil/StringUtil.java
@@ -5,24 +5,18 @@ package com.test.study.util.StringUtil;
  */
 public class StringUtil {
 
-    public static boolean isEmpty(String s) {
-        if (s == null) {
-            return true;
-        }
-        if (s.trim().equals("")) {
-            return true;
-        }
-        return false;
-    }
+	public static boolean isEmpty(String s) {
+		return s == null || s.equals("");
+	}
 
-    public static boolean isEmpty(String[] ss){
-        for (String s:ss ){
-            if (isEmpty(s)){
-                return true;
-            }
-        }
-        return false;
-    }
+	public static boolean isEmpty(String[] ss) {
+		for (String s : ss) {
+			if (isEmpty(s)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    
+
 }
