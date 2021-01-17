84d13f4a99b2cb4e51b9ebd06752a5f6521ef6cc
wlm2012 <541253785@qq.com>
2021-01-17 16:37:30 +0800

    CoffeeMapperController

--- src/main/java/com/test/study/entity/CoffeePlus.java
+++ src/main/java/com/test/study/entity/CoffeePlus.java
@@ -4,20 +4,19 @@ package com.test.study.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.Column;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@TableName("t_menu")
+@TableName("t_order_plus")
 @Data
+@Builder
+@AllArgsConstructor
 public class CoffeePlus {
 
 	@TableId(type = IdType.AUTO)
@@ -29,9 +28,7 @@ public class CoffeePlus {
 	@NotNull
 	private BigDecimal price;
 
+	private LocalDateTime createTime;
 
-	private LocalDateTime creatTime;
-
-	@Column(name = "updateTime")
 	private LocalDateTime updateTime;
 }
--- src/main/java/com/test/study/geektimeSpringFamily/Chapter6/CoffeeMapperController.java
+++ src/main/java/com/test/study/geektimeSpringFamily/Chapter6/CoffeeMapperController.java
@@ -4,18 +4,24 @@ package com.test.study.geektimeSpringFamily.Chapter6;
 import com.test.study.entity.CoffeePlus;
 import com.test.study.primaryMapper.CoffeeMapper;
 import lombok.extern.slf4j.Slf4j;
+import net.bytebuddy.agent.builder.AgentBuilder;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/coffee")
@@ -36,14 +42,41 @@ public class CoffeeMapperController {
 	public CoffeePlus addCoffee(@Valid CoffeePlus coffeePlus, BindingResult result) {
 		if (result.hasErrors()) {
 			log.warn("erroes :{}", result);
+//			int a=0/0;
 		}
 
-		coffeePlus.setCreatTime(LocalDateTime.now());
+		coffeePlus.setCreateTime(LocalDateTime.now());
 		coffeePlus.setUpdateTime(LocalDateTime.now());
 		if (1 == coffeeMapper.insert(coffeePlus)) {
 			log.info("success");
 		}
 		return coffeePlus;
+	}
+
+
+	@PostMapping(path = "/", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	@ResponseBody
+	@ResponseStatus(HttpStatus.CREATED)
+	public List<CoffeePlus> addCoffee(@RequestParam("file") MultipartFile file) {
+		List<CoffeePlus> list = new ArrayList<>();
+		if (!file.isEmpty()) {
+			try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+				String str;
+				while ((str = reader.readLine()) != null) {
+
+//					StringUtils.split()
+					String[] arr = str.split(" ");
+					if (arr.length == 2) {
+						CoffeePlus coffeePlus = CoffeePlus.builder().name(arr[0]).price(new BigDecimal(arr[1])).build();
+						coffeeMapper.insert(coffeePlus);
+						list.add(coffeePlus);
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
 	}
 
 
