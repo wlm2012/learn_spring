f67b4aed1efe03fb6830ab31f689bac13dc46bee
wlm2012 <541253785@qq.com>
2021-01-22 17:54:59 +0800

    Syntax

---
+++ src/main/resources/templates/Dom-JavaScript/Syntax.js
@@ -0,0 +1,33 @@
+mood = "sad";
+age = 28;
+/*
+alert(age);
+alert(mood);
+alert(age + 1);*/
+
+mood = "the 5'10\" ";
+// alert(mood);
+
+var beats = Array();
+
+beats[0] = "John";
+beats[2] = "Smith";
+// alert(beats);
+
+
+// false
+/*if (a = false) {
+    alert("a=false");
+}*/
+
+
+function f(num) {
+    var init = 0;
+    while (init < num) {
+        alert(init);
+        init++;
+    }
+}
+
+f(4);
+f(2);
