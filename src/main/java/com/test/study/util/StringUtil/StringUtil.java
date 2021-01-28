fda77149bf6fda99756a6b0cd5597af50ab3bda9
wlm2012 <541253785@qq.com>
2021-01-28 11:34:51 +0800

    StringUtil

--- src/main/java/com/test/study/util/StringUtil/StringUtil.java
+++ src/main/java/com/test/study/util/StringUtil/StringUtil.java
@@ -1,5 +1,7 @@
 package com.test.study.util.StringUtil;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * StringUtil
  */
@@ -16,6 +18,15 @@ public class StringUtil {
 			}
 		}
 		return false;
+	}
+
+	public static boolean isEmpty2(String s) {
+
+		return StringUtils.isEmpty(s);
+	}
+
+	public static boolean isBlank(String s) {
+		return StringUtils.isBlank(s);
 	}
 
 
